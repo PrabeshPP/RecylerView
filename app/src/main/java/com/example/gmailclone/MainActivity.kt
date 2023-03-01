726fff4b5a73fea60cbbbb1dafef9ba71de53e6c
PrabeshPP <prabesh.bista369@gmail.com>
2023-02-28 21:40:00 -0500

    [completed] required features

--- app/src/main/java/com/example/gmailclone/MainActivity.kt
+++ app/src/main/java/com/example/gmailclone/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.gmailclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var emails: List<Email>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val recylerViewId=findViewById<RecyclerView>(R.id.recyler1)
+        emails = EmailFetcher.getEmails()
+        // Create adapter passing in the list of emails
+        val adapter = EmailAdapter(emails)
+        // Attach the adapter to the RecyclerView to populate items
+        recylerViewId.adapter = adapter
+        // Set layout manager to position the items
+        recylerViewId.layoutManager = LinearLayoutManager(this)
     }
 }
